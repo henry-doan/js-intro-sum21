a4ec074da330c5ea35574aa3828451b9be35e184
Henry Doan <henrydoan96@gmail.com>
2021-05-19 20:51:12 -0600

    end of intro

---
+++ notes.js
@@ -0,0 +1,72 @@
+// // Comment 
+
+// // Datatypes
+// integer
+// boolean
+// float
+// string
+// array
+// hash, objects
+// null
+// NAN 
+// undefined
+
+// + - * % / ( ) 
+// < > == === != <= >= && ||
+/* ++
+-- += -=  */
+// console.log("hello")
+
+// if ("bob" != firstName) {
+//   console.log("hellow")
+// } else {
+//   console.log("bye")
+// }
+
+// while (num != 5) {
+  
+// }
+
+// variables 
+// var firstName = "bob"
+
+// let firstName = "bob"
+// firstName = "jill"
+// const lastName = "smith"
+// lastName = "lo"
+// null
+
+// function home(text) {
+
+// }
+
+// home("text")
+
+// const - write function, vars, module 
+
+// const home = (text, age) => {
+
+// }
+
+// home("hellow")
+
+// onClick={() => { console.log("hello") }}
+
+// module / components 
+// class HomePage
+// const HomePage = () => {
+//   return (
+
+//   )
+// }
+
+// const HomePage = () => (
+
+// )
+
+// export default HomePage 
+// export Aboutpage 
+
+// // in another file 
+// import HomePage from './Homepage'
+// import { Aboutpage } from './Aboutpage'
